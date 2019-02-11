745cb385d3d208b00993db29849b1fba1b4f7de8
Hans van Meurs <hans@androidappfactory.nl>
2019-02-11 19:40:21 +0100

    Init commit

---
+++ src/main/java/nl/aaf/model/UserCommand.java
@@ -0,0 +1,17 @@
+package nl.aaf.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Builder
+
+public class UserCommand {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
