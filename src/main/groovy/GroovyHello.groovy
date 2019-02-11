80eb90a1da2442224bb95a0d7a8c9600b6888c51
Hans van Meurs <hans@androidappfactory.nl>
2019-02-11 22:23:03 +0100

    Combined Java and Groovy in one maven project

---
+++ src/main/groovy/GroovyHello.groovy
@@ -0,0 +1,13 @@
+import nl.androidappfactory.JavaHelloWorld
+
+class GroovyHello {
+
+
+    public static void main(String[] args) {
+
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld()
+
+        println(javaHelloWorld.getHello("Hans"));
+    }
+
+}
--- src/main/java/nl/androidappfactory/JavaHelloWorld.java
+++ src/main/java/nl/androidappfactory/JavaHelloWorld.java
@@ -1,9 +1,14 @@
 package nl.androidappfactory;
 
-public class HelloWorld {
+public class JavaHelloWorld {
 
     public static void main(String[] args) {
 
         System.out.println("Hallooooootjes wereldbol");
+    }
+
+    public String getHello(String name) {
+
+        return ("Hello " + name);
     }
 }
