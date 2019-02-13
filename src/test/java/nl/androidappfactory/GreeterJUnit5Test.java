97bf1460b7ead9ba46f2f4c7c9daa8a21f97393e
Hans van Meurs <hans@androidappfactory.nl>
2019-02-13 18:08:10 +0100

    Doing some JUnit4, JUnit5 and Spock tests

---
+++ src/test/java/nl/androidappfactory/GreeterJUnit5Test.java
@@ -0,0 +1,20 @@
+package nl.androidappfactory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreeterJUnit5Test {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void sayHello() {
+
+        assertTrue("a".equals("a"));
+    }
+}
