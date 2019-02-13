9c4751f543b5a6f7287ef9e2405e2c9d976291f0
Hans van Meurs <hans@androidappfactory.nl>
2019-02-13 17:42:07 +0100

    Removed spock --> to other branch

--- src/test/groovy/nl/androidappfactory/GreeterTest.groovy
+++ src/test/groovy/nl/androidappfactory/GreeterTest.groovy
@@ -1,37 +1,7 @@
 package nl.androidappfactory
 
-import spock.lang.Specification
-
-class GreeterTest extends Specification {
-    void setup() {
-    }
-
-    void cleanup() {
-    }
-
-    def "SayHello With given name"() {
-
-        given:
-        def greeter = new Greeter()
-        def name = "Hans"
-
-        when:
-        def greet = greeter.sayHello(name)
-
-        then:
-        greet.equals("Hallo Hans!")
-    }
-
-    def "SayHello Without given name"() {
-
-        given:
-        def greeter = new Greeter()
-        def name = null
-
-        when:
-        def greet = greeter.sayHello(name)
-
-        then:
-        greet.equals("Hallo guys!")
+class GreeterTest extends GroovyTestCase {
+    void testSayHello() {
+        assertTrue(true)
     }
 }
